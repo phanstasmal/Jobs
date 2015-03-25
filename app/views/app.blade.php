71abd603ed97f4ae5b8fdcb7bcc6d2bf4f0ed4c1
phanstasmal <phanstasmal@gmail.com>
2015-03-25 15:43:17 +0700

    Fix login

--- app/views/app.blade.php
+++ app/views/app.blade.php
@@ -39,19 +39,19 @@
 					<li class="active"><a href="{{url('/')}}">Home <span class="sr-only">(current)</span></a></li>
 				</ul>
 
-				<ul class="nav navbar-nav navbar-right">
-					@if (Auth::guest())
-						<li><a href="/auth/login">Login</a></li>
-						<li><a href="/auth/register">Register</a></li>
-					@else
-						<li class="dropdown">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
-							<ul class="dropdown-menu" role="menu">
-								<li><a href="/auth/logout">Logout</a></li>
-							</ul>
-						</li>
-					@endif
-				</ul>
+				{{--<ul class="nav navbar-nav navbar-right">--}}
+					{{--@if (Auth::guest())--}}
+						{{--<li><a href="/auth/login">Login</a></li>--}}
+						{{--<li><a href="/auth/register">Register</a></li>--}}
+					{{--@else--}}
+						{{--<li class="dropdown">--}}
+							{{--<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>--}}
+							{{--<ul class="dropdown-menu" role="menu">--}}
+								{{--<li><a href="/auth/logout">Logout</a></li>--}}
+							{{--</ul>--}}
+						{{--</li>--}}
+					{{--@endif--}}
+				{{--</ul>--}}
 			</div><!-- /.navbar-collapse -->
 		</div><!-- /.container-fluid -->
 	</nav>
